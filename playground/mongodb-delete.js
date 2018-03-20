c6b9a0b82be32fd94e4104de2c9494e75a9f6b9a
Fady Sameh <fady97sameh@gmail.com>
2018-03-20 23:59:48 +0200

    added delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,22 @@
+const {MongoClient, ObjectID} =require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('unable to connect');
+  }
+  console.log('connection successful');
+
+  //deleteMany
+  // db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+  //   console.log(result.result);
+  // });
+  //deleteOne
+  // db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result) => {
+  //   console.log(result.result);
+  // });
+  //findOneAndDelelete
+  db.collection('Todos').findOneAndDelete({text: 'eat lunch'}).then((result) => {
+    console.log(result);
+  });
+  db.close();
+});
