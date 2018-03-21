b1a0ba039d7779c3c4cde4a106bb656e35fa8281
Fady Sameh <fady97sameh@gmail.com>
2018-03-21 22:28:30 +0200

    post todo

--- server/server.js
+++ server/server.js
@@ -10,8 +10,15 @@ var {User} = require('../models/user');
  app.use(bodyParser.json());
 
  app.post('/todos', (req, res) => {
-   console.log(res);
- })
+   var newTodo = new Todo({
+     text: req.body.text
+   });
+   newTodo.save().then((doc) => {
+     res.send(doc);
+   }, (err) => {
+     res.status(400).send(err);
+   });
+ });
 
   app.listen(3000, () => {
     console.log("Server running on port 3000");
