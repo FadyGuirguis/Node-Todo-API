b017dd72dce108e9d77ca26620e15b8f3e383972
Fady Sameh <fady97sameh@gmail.com>
2018-03-25 00:56:47 +0200

    adeed GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,24 @@ var {User} = require('./models/user');
      });
    }).catch((err) => {
      res.status(400).send(err);
+   });
+ });
+
+ app.get('/todos/:id', (req, res) => {
+   if (!ObjectId.isValid(req.params.id)) {
+     return res.status(404).send({
+       error: 'todo not found'
+     });
+   }
+   Todo.findById(req.params.id).then((todo) => {
+     if (!todo) {
+       return res.status(404).send({
+         error: 'todo not found'
+       });
+     }
+     res.send(todo);
+   }).catch((err) => {
+     res.status(400).send({});
    });
  });
 
